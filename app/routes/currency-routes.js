de6bed9d74502ef3839e04b530b9ff1303c6dd67
Francis Boily <boilyfrank28@gmail.com>
2017-11-04 00:25:29 -0400

    Last push did fall into the abyss

--- app/routes/currency-routes.js
+++ app/routes/currency-routes.js
@@ -2,6 +2,6 @@ module.exports = function(app, db) {
     app.get('/currency', (req, res) => {
         console.log('CALLED');
         res.type('json')
-        res.send('{"price": "398"}');
+        res.send('{"price": "397"}');
       });
 };
